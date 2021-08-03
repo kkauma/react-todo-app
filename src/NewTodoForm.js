dd2b606417de7f1bc8a0b6abae2fa5a77470d5b5
Kyle Kauma <kylekauma@gmail.com>
2021-08-02 22:03:07 -0500

    add remove todo functionality

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 class NewTodoForm extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class NewTodoForm extends Component {
   }
   handleSubmit(evt) {
     evt.preventDefault();
-    this.props.createTodo(this.state);
+    this.props.createTodo({ ...this.state, id: uuidv4() });
     this.setState({ task: "" });
   }
   render() {
--- src/TodoList.js
+++ src/TodoList.js
@@ -6,21 +6,31 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        { task: "Code in JavaScript" },
-        { task: "Go to the driving range" },
-      ],
+      todos: [],
     };
     this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
   }
   create(newTodo) {
     this.setState({
       todos: [...this.state.todos, newTodo],
     });
   }
+  remove(id) {
+    this.setState({
+      todos: this.state.todos.filter((t) => t.id !== id),
+    });
+  }
   render() {
     const todos = this.state.todos.map((todo) => {
-      return <Todo task={todo.task} />;
+      return (
+        <Todo
+          key={todo.id}
+          id={todo.id}
+          task={todo.task}
+          removeTodo={this.remove}
+        />
+      );
     });
     return (
       <div>
